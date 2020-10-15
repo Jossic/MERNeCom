3fedcd1282ceead491b8b040e145ae19542c4010
Jossic <jossic.lapierre@gmail.com>
2020-10-15 22:33:23 +0200

    MERN eCom - 6 - HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from "react-bootstrap";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <h1>Bienvenue sur Baby-Hoshi</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />

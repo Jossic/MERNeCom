efc0517f8544f2164a5161797427d2e574d7c281
Jossic <jossic.lapierre@gmail.com>
2020-10-17 14:17:18 +0200

    MERN eCom - 11 - Serving Products - Backend routes

---
+++ backend/data/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Pyjama',
+    image: '/images/1.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatibus animi laboriosam quis harum minima facilis? Provident adipisci odit exercitationem. Voluptas natus veritatis tempora ab numquam vero, ullam qui. Voluptates praesentium itaque deleniti, minus reprehenderit iusto accusantium nisi saepe ipsa? Ducimus optio delectus, obcaecati iste magnam officia laudantium quam.',
+    brand: 'Apple',
+    category: 'Pyjama',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Chaussures',
+    image: '/images/2.jpg',
+    description:
+      'Inlorem',
+    brand: 'Apple',
+    category: 'Chaussures',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Body fille',
+    image: '/images/3.jpg',
+    description:
+      'lorem',
+    brand: 'Cannon',
+    category: 'Body',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Pyjama bleu garçon',
+    image: '/images/4.png',
+    description:
+      'lorem',
+    brand: 'Sony',
+    category: 'Pyjama',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Body fille a fleur',
+    image: '/images/5.png',
+    description:
+      'lorem',
+    brand: 'Logitech',
+    category: 'Body',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Lot 3 body',
+    image: '/images/6.png',
+    description:
+      'lorme',
+    brand: 'Amazon',
+    category: 'body',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '7',
+    name: 'Porte bébé marche',
+    image: '/images/7.jpg',
+    description:
+      'lorem',
+    brand: 'Amazon',
+    category: 'Eveil & Jeux',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products;

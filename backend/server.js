de0271b5df76f4d60614507a86ff8b0fe875da12
Jossic <jossic.lapierre@gmail.com>
2020-10-17 14:43:23 +0200

    MERN eCom - 14 - Environment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const products = require('./data/products');
 
 const app = express();
@@ -17,5 +18,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 })
 
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server runnning on port 5000...'))
+app.listen(PORT, console.log(`Server runnning in ${process.env.NODE_ENV} on port ${PORT}...`))

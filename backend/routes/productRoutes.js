0a0acf79481e23c64719a0329e2fb59e12c47ef6
Jossic <jossic.lapierre@gmail.com>
2020-10-17 22:22:17 +0200

    MERN eCom - 22 - Fetching Products From The Database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import colors from 'colors';
+import AsyncHandler from 'express-async-handler';
+const router = express.Router()
+import Product from '../models/productModel.js';
+
+// @desc      Fetch all products
+// @route     GET /api/products
+// @access    Private
+router.get('/', AsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+}))
+
+
+// @desc      Fetch product by id
+// @route     GET /api/products/:id
+// @access    Public
+router.get('/:id', AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({ message: 'Produit non trouvé' })
+    }
+    res.json(product);
+}))
+
+
+export default router;

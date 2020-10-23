c59507ecbe6bde1bdf7a890930a5df43169c0b8e
Jossic <jossic.lapierre@gmail.com>
2020-10-23 21:10:59 +0200

    MERN eCom - 36 - Clean Up By Using Controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import AsyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// @desc      Fetch all products
+// @route     GET /api/products
+// @access    Private
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+})
+
+// @desc      Fetch product by id
+// @route     GET /api/products/:id
+// @access    Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Produit non trouvé');
+    }
+})
+
+export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,10 @@
 import express from 'express';
-import colors from 'colors';
-import AsyncHandler from 'express-async-handler';
 const router = express.Router()
-import Product from '../models/productModel.js';
-
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Private
-router.get('/', AsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}))
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 
-// @desc      Fetch product by id
-// @route     GET /api/products/:id
-// @access    Public
-router.get('/:id', AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Produit non trouvé');
-    }
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 
 export default router;

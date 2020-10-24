22a771f52276556c949468e37d022714d3d87287
Jossic <jossic.lapierre@gmail.com>
2020-10-24 13:22:41 +0200

    MERN eCom - 37 - User Authentication Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import Product from '../models/productModel.js';
 // @desc      Fetch all products
 // @route     GET /api/products
 // @access    Private
-const getProducts = asyncHandler(async (req, res) => {
+const getProducts = AsyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
 })
@@ -12,7 +12,7 @@ const getProducts = asyncHandler(async (req, res) => {
 // @desc      Fetch product by id
 // @route     GET /api/products/:id
 // @access    Public
-const getProductById = asyncHandler(async (req, res) => {
+const getProductById = AsyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {

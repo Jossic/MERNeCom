0d5ba859a666f53c46c5b816b20c4b265d342857
Jossic <jossic.lapierre@gmail.com>
2020-10-30 22:01:23 +0100

    MERN eCom - 62 - Order Pay Reducer & Action

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,10 +1,10 @@
-import AsyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
 // @desc      Create new Order
 // @route     POST /api/orders
 // @access    Private
-const addOrderItems = AsyncHandler(async (req, res) => {
+const addOrderItems = asyncHandler(async (req, res) => {
     const {
         orderItems,
         shippingAddress,
@@ -36,11 +36,29 @@ const addOrderItems = AsyncHandler(async (req, res) => {
     }
 })
 
-// @desc      Get order by ID
-// @route     POST /api/orders/:id
-// @access    Private
-const getOrderById = AsyncHandler(async (req, res) => {
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
+
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Commande non trouvée')
+    }
+})
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id)
+
     if (order) {
         order.isPaid = true
         order.paidAt = Date.now()
@@ -48,25 +66,12 @@ const getOrderById = AsyncHandler(async (req, res) => {
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
-            email_address: req.body.payer.email_address
+            email_address: req.body.payer.email_address,
         }
 
         const updatedOrder = await order.save()
 
         res.json(updatedOrder)
-    } else {
-        res.status(404)
-        throw new Error('Commande non trouvée')
-    }
-})
-
-// @desc      Update order to paid
-// @route     POST /api/orders/:id/pay
-// @access    Private
-const updateOrderToPaid = AsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email');
-    if (order) {
-        res.json(order)
     } else {
         res.status(404)
         throw new Error('Commande non trouvée')

3629182872694bea49f2704edd5a1d8e1979f351
Jossic <jossic.lapierre@gmail.com>
2020-11-02 15:31:23 +0100

    MERN eCom - 75 - Create & Update Product Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,10 @@
-import AsyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 // @desc      Fetch all products
 // @route     GET /api/products
 // @access    Private
-const getProducts = AsyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
 })
@@ -12,7 +12,7 @@ const getProducts = AsyncHandler(async (req, res) => {
 // @desc      Fetch product by id
 // @route     GET /api/products/:id
 // @access    Public
-const getProductById = AsyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
@@ -26,7 +26,7 @@ const getProductById = AsyncHandler(async (req, res) => {
 // @desc      Delete a product
 // @route     DELETE /api/products/:id
 // @access    Private/Admin
-const deleteProduct = AsyncHandler(async (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
@@ -38,4 +38,59 @@ const deleteProduct = AsyncHandler(async (req, res) => {
     }
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc      Create a product
+// @route     POST /api/products
+// @access    Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = await Product({
+        name: 'Name',
+        price: 0,
+        user: req.user._id,
+        image: '/image/sample.jpg',
+        brand: 'Brand',
+        category: 'Cat',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Desc',
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+// @desc      Update a product
+// @route     PUT /api/products/:id
+// @access    Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name,
+        price,
+        description,
+        image,
+        brand,
+        category,
+        countInStock }
+        = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Produit non trouvé');
+    }
+
+
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }

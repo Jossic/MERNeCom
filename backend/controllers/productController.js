d4b0a2460ec59459faef58309d8d7833a7da8bdc
Jossic <jossic.lapierre@gmail.com>
2020-11-02 15:02:46 +0100

    MERN eCom - 74 - Admin Delete Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,19 @@ const getProductById = AsyncHandler(async (req, res) => {
     }
 })
 
-export { getProducts, getProductById }
+// @desc      Delete a product
+// @route     DELETE /api/products/:id
+// @access    Private/Admin
+const deleteProduct = AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Produit supprimé' })
+    } else {
+        res.status(404)
+        throw new Error('Produit non trouvé');
+    }
+})
+
+export { getProducts, getProductById, deleteProduct }

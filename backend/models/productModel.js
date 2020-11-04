6eb4e13ff9acd462de515696da820f060e0920fa
Jossic <jossic.lapierre@gmail.com>
2020-11-04 14:35:18 +0100

    MERN eCom - 84 - Front End Product Reviews

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,18 +1,9 @@
 import mongoose from 'mongoose';
 
 const reviewSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: true
-    },
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,7 +14,10 @@ import {
     PRODUCT_CREATE_FAIL,
     PRODUCT_UPDATE_REQUEST,
     PRODUCT_UPDATE_SUCCESS,
-    PRODUCT_UPDATE_FAIL
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -136,6 +139,36 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     } catch (e) {
         dispatch({
             type: PRODUCT_UPDATE_FAIL,
+            payload: e.response && e.response.data.message ? e.response.data.message : e.message
+        })
+    }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_SUCCESS,
+        })
+    } catch (e) {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAIL,
             payload: e.response && e.response.data.message ? e.response.data.message : e.message
         })
     }
